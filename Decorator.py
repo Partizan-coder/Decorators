56e9c5835537c16a71e7ec81d8e7042ece8a0092
Partizan-coder <79650117+Partizan-coder@users.noreply.github.com>
2021-09-14 22:34:08 +0300

    Update_decorator

--- Decorator.py
+++ Decorator.py
@@ -3,11 +3,14 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+
 filepath = os.path.join(os.getcwd())
 
 def logger(_filepath):
 
+
     def habr(function):
+
         nonlocal _filepath
 
         func_name = None
@@ -17,6 +20,7 @@ def logger(_filepath):
         func_result = None
 
         def replaced_func(*args, **kwargs):
+
             nonlocal func_name
             nonlocal func_time
             nonlocal func_args
@@ -27,41 +31,40 @@ def logger(_filepath):
             func_args = args
             func_kwargs = kwargs
             func_result = function(*args, **kwargs)
-
-            return f'Результат работы декоратора: \n' \
+#
+            data = f'Результат работы декоратора: \n' \
                    f'Имя функции:    {func_name}  \n' \
                    f'Время запуска функции:    {func_time} \n' \
                    f'Аргументы функции:    {func_args}    {func_kwargs} \n' \
                    f'Результат работы функции:    {func_result}'
 
-        with open(f'{_filepath}\\result.txt', 'w') as f:
-            date = replaced_func()
-            f.write(date)
-        f.close()
-
+            with open(f'{_filepath}\\result.txt', 'w') as f:
+                f.write(data)
+            f.close()
         return replaced_func
-
     return habr
 
 @logger(filepath)
-def habrParser():
-    response = requests.get('https://habr.com/ru/all/')
-    KEYWORDS = {'дизайн', 'фото', 'Программирование', 'python', 'Сетевое оборудование'}
-    href = 'Ссылка по умолчанию: https://habr.com/ru/all/'
+def habrparser():
+
+    response = requests.get('https://habr.com/ru/all')
+    KEYWORDS = {'IT-компании', 'Блог компании Pixonic'}
+    href = []
 
     if not response.ok:
         raise ValueError('No response')
-
+    else:
         text = response.text
         soup = BeautifulSoup(text, features="html.parser")
         articles = soup.find_all('article')
         for article in articles:
             hubs = {h.text for h in article.find_all('a', class_='tm-article-snippet__hubs-item-link')}
             if KEYWORDS & hubs:
-                time_post = article.find(class_="tm-article-snippet__datetime-published").time.get('title')[:10]
-                header = article.find(class_="tm-article-snippet__title tm-article-snippet__title_h2").text
-                href = article.find('a', class_="tm-article-snippet__title-link").attrs.get('href')
+                # time_post = article.find(class_="tm-article-snippet__datetime-published").time.get('title')[:10]
+                # header = article.find(class_="tm-article-snippet__title tm-article-snippet__title_h2").text
+                href.append(article.find('a', class_="tm-article-snippet__title-link").attrs.get('href'))
     return href
 
+
 if __name__ == '__main__':
-    habrParser()
+    habrparser()
